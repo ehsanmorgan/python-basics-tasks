1282abe1c47870dba118e9deb698ef889fdef594
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-11-08 14:24:41 +0100

    task2

---
+++ 02-operators/tasks2.py
@@ -0,0 +1,111 @@
+'''
+python operators tasks
+
+'''
+
+#'1 what is defference between =,==
+'''
+  = to great a variable
+  == question
+'''
+
+#2 what do you mean by using ! ?
+'''
+! using by question
+
+'''
+
+#3 what is the operator precedence
+
+'''
+( )
+**
+* /
++ -
+
+'''
+
+#4 Create a variable x with value of 10
+x=10
+
+#5  increase x value by 15 using assignmet operator
+
+x=10
+x
+10
+x += 5
+x
+15
+
+#6  divide x value by 5 using assignmet operator
+
+x=15
+x /=3
+x=5
+
+#7  multiply x value by 10 using assignmet operator
+
+x=5
+x *=2
+x=10
+
+#8  get module of x on 3  using assignmet operator
+
+x%=3
+x
+1.0
+
+#9 using python print the module of 11 to 4
+
+11%4
+3
+
+#10 print the exponent of 2.3
+
+2**3
+8
+
+#11 divide 11 by 3 using python division
+
+11/3
+3.6666666666666665
+
+#12 Can we divide 11 y 3 and get an integer number
+
+11//3
+3
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
