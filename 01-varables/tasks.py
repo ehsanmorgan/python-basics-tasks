e295b6924b1ae2def0e30cb71a7a4ca815922d28
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-11-08 13:25:02 +0100

    add tasks

---
+++ 01-varables/tasks.py
@@ -0,0 +1,57 @@
+'''
+python variables tasks
+
+'''
+
+
+
+# 1 create a Boolean variable named x
+x = True
+
+#2 create an integer variable named y
+y=4
+
+#3 Great a float  variable named z
+z= 5.2 
+
+#4 Great a streng  variable named s
+s=' Ehsan '
+
+#5 convert the int variable to float
+y=4.2
+
+#6 Can we convert the streng to intege ?
+Yes
+
+#7 Creat a list of numbers from 1 to 5
+w= [1,2,3,4,5 ]
+
+#8 Creat a tuble of numbers from 10 to 15
+k=(10,11,12,13,14,15)
+
+#9 convert the list to tuble
+w=(1,2,3,4,5)
+
+#10 creat a dict of 3 values
+E={'ehsan' :12 , 'Ahmad':30 }
+
+#12 python is interpreted or compiled ?
+'''
+python is interpreted 
+'''
+#13 what is differences between low level and high level ?
+
+'''
+low level is like C the first programming language
+high level is like java script and python
+
+'''
+
+
+
+
+
+
+
+
+
