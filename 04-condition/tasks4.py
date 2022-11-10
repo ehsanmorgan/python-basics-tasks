3261aa3681d8d767436ee4bb0be11db7016b4790
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-11-10 13:24:25 +0100

    tasks4

---
+++ 04-condition/tasks4.py
@@ -0,0 +1,149 @@
+'''
+python coundition tasks
+
+'''
+
+#2 if 10 is not bigger than 15 print x is smaller than 15
+
+x=10
+
+if x<15 :
+      print ('x is smaller than 15')
+
+
+#3 in wiche cases we will use all
+
+'''
+when we  have more than one (and)
+
+'''
+
+#4 what is the defernce between and ,all
+
+
+
+#5 what is the defernce between or ,any
+
+
+
+
+#6 if we need all the condition to be true we will use ?
+
+'''
+if we have to be all condition true , we will use (and ,all)
+
+'''
+
+
+
+#7 what is ehr deferece between if elif
+
+'''
+if is the main condition and can just used one time
+
+elif can used more than one time in the same condition
+
+'''
+
+
+#8 what is defrence between elif , else
+
+'''
+else can used one time
+
+elif can used more than one time
+
+'''
+
+
+
+
+#9 can we use more than elif ?
+
+'''
+yes
+
+'''
+
+#10 can we use more than else ?
+
+'''
+No
+
+'''
+
+
+
+#11 write a simple ternary operator
+username='ehsan'
+print ('username=ehsan')  if username== 'ehsan'  else print ('error')
+
+username='ahmad'
+print ('username=ehsan')  if username== 'ehsan'  else print ('error')
+
+
+#12 in elif python will check all the conditions no matter what ?
+
+'''
+YES
+'''
+
+
+#13 in elif we will use else for ?
+
+
+
+#14 if we have the list [2,4,6,8,19]
+
+#1 check to see if 2 in this list or not
+
+
+x=(2,4,6,8,10)
+
+if 4 in x: print('4 in x')
+
+
+
+#2 check to see if 2 and 4  in this list or not
+
+if 4 and 6 in x :print ('done')
+
+
+
+#3 check to see if 3 or 6 in this list or not
+
+if 3 or 6 in x: print ('done')
+
+
+#4 check to see if 2,4 and 5 in this list or not
+
+if all ([2 in x , 4 in x , 5 in x]) : print('all number in this list') 
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
