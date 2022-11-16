2f96bd5e9d97314df67c37ac395f3e298631c1f1
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-11-16 17:28:00 +0100

    tasks 5

---
+++ 05-loops/loops tasks.py
@@ -0,0 +1,1840 @@
+Python 3.11.0 (v3.11.0:deaf509e8f, Oct 24 2022, 14:43:23) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
+Type "help", "copyright", "credits" or "license()" for more information.
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for x in 11 :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for x in (11) :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for y in (x) :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for y in (11) :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for y in x :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for y in x :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for x in list :
+TypeError: 'type' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 13, in <module>
+    for x in list :
+TypeError: 'type' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+11
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1 X 0 =0
+2 X 0 =0
+2 X 1 =2
+3 X 0 =0
+3 X 1 =3
+3 X 2 =6
+4 X 0 =0
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+5 X 0 =0
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+range (10,21)
+range(10, 21)
+range list[10,21]
+SyntaxError: invalid syntax
+range ( list[10,20])
+Traceback (most recent call last):
+  File "<pyshell#2>", line 1, in <module>
+    range ( list[10,20])
+TypeError: 'types.GenericAlias' object cannot be interpreted as an integer
+list(range(10,21))
+[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+list(range(10,100,3))
+[10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97]
+list(range(10,103,3))
+[10, 13, 16, 19, 22, 25, 28, 31, 34, 37, 40, 43, 46, 49, 52, 55, 58, 61, 64, 67, 70, 73, 76, 79, 82, 85, 88, 91, 94, 97, 100]
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+1
+100
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+2
+101
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 52, in <module>
+    for y in (x):
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+102
+102
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 52, in <module>
+    for x in (numbers):
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 52, in <module>
+    for x in (101):
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+n
+u
+m
+b
+e
+r
+s
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 51, in <module>
+    for x in (100):
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 51, in <module>
+    for x in (100):
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+11
+12
+13
+14
+15
+16
+17
+18
+19
+20
+21
+22
+23
+24
+25
+26
+27
+28
+29
+30
+31
+32
+33
+34
+35
+36
+37
+38
+39
+40
+41
+42
+43
+44
+45
+46
+47
+48
+49
+50
+51
+52
+53
+54
+55
+56
+57
+58
+59
+60
+61
+62
+63
+64
+65
+66
+67
+68
+69
+70
+71
+72
+73
+74
+75
+76
+77
+78
+79
+80
+81
+82
+83
+84
+85
+86
+87
+88
+89
+90
+91
+92
+93
+94
+95
+96
+97
+98
+99
+100
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 52, in <module>
+    for y in x :
+TypeError: 'int' object is not iterable
+
+= RESTART: /Users/fatemaabdallah/Documents/adas.py
+0
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+5
+6
+7
+8
+9
+10
+1
+2
+3
+4
+1
+2
+3
+4
+6
+7
+8
+9
+10
+1 X 1 =1
+1 X 2 =2
+1 X 3 =3
+1 X 4 =4
+1 X 5 =5
+2 X 1 =2
+2 X 2 =4
+2 X 3 =6
+2 X 4 =8
+2 X 5 =10
+3 X 1 =3
+3 X 2 =6
+3 X 3 =9
+3 X 4 =12
+3 X 5 =15
+4 X 1 =4
+4 X 2 =8
+4 X 3 =12
+4 X 4 =16
+4 X 5 =20
+5 X 1 =5
+5 X 2 =10
+5 X 3 =15
+5 X 4 =20
+5 X 5 =25
+Traceback (most recent call last):
+  File "/Users/fatemaabdallah/Documents/adas.py", line 52, in <module>
+    for y in (x) :
+TypeError: 'int' object is not iterable
