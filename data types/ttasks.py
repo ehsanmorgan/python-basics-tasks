8ebb24399ae7e7b0ac249a0951ce133a1dac1043
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-11-17 16:04:01 +0100

    add tasks

---
+++ data types/ttasks.py
@@ -0,0 +1,107 @@
+#1 what is the difference between ' " ''' ?
+
+'''
+is no difference between single and double quoation
+just by trible quoation we use it when we want to  write the sentence in second row
+
+Example :
+
+'''
+
+print (''' ich bin ehsan
+ich komme aus syria ''')
+
+#2 Creat a srting with the name 'mysrto'
+
+username =('mystro')
+print(username)
+
+#3 Make the string letters capital
+
+username.upper()
+'MYSTRO'
+
+#4 Cerat a list of values from 10 to 20
+
+l=[10,11,12,13,14,15,16,17,18,19,20]
+
+#5 Add   30 to the end of the list
+
+l.append(30)
+l
+[10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 30]
+
+
+
+#6 Add 40 s the second element of the list 
+
+
+
+l.insert(1,40)
+l
+[30, 40, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10]
+
+
+#7 Print how many element in the list 
+
+
+
+
+
+
+
+#8 replace the second element in the list with 100 
+
+
+l[1]
+40
+l[1]=100
+l
+[30, 100, 20, 19, 18, 17, 16, 15, 14, 13, 12, 11, 10]
+
+
+
+
+#9 Creat a tuple with valeu from 1 to5
+
+t=(1,2,3,4,5)
+
+
+
+#10 can we add 10 to the end of the tuple
+
+'''
+no we can't add it but we can convert the tuple to the list and we add it.
+
+'''
+
+#11 Creat a dict of value mahmoud :28 , ahmed:30
+
+
+d = {'mahmoud' : 28, 'ahmad': 30 }
+
+#12 Print Mahmoud age from the dict
+
+d = {'mahmoud' : 28, 'ahmad': 30 }
+
+for x in d .values():
+      print(x)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
