fb620b02ff63867bce7ba9166dd448812dbcf237
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-12-02 19:18:47 +0100

    add

---
+++ new_tasks2/tasks3.py
@@ -0,0 +1,50 @@
+def tri_area(base, height,x):
+      result=(base*height/x)
+      return result
+	
+print(tri_area(21,32,2))
+
+
+
+def bool_to_string(flag):
+              return flag
+print (str(bool_to_string('e')))
+
+
+
+def string_int(txt):
+	return (txt)
+
+print(string_int('r'))
+
+
+
+
+def animals(chickens, cows, pigs):
+	result=(chickens*2+cows*4+pigs*4)
+	return result
+	
+print(animals(2,54,4))
+
+
+def convert(hours, minutes):
+	result=(60*60*hours+ 60*minutes)
+	return result
+	
+print(convert(5,30))
+
+
+
+
+
+
+
+def total_amount_adjectives(dct):
+              adjectives={'idiot':2,'moron':5}
+              return adjectives
+
+print(total_amount_adjectives(1))
+
+
+
+
