09fb847789f7796bfa9e9ba2acb6cce1fa5acc5a
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-12-06 19:39:32 +0100

    test hackerrank

---
+++ new_tasks2/Hackerrank.py
@@ -0,0 +1,93 @@
+
+def is_leap(year):
+      
+      leap=False
+      if year in range(1900, 10**5+1):
+            if year%4==0:
+                  leap=True
+                  if year%400==0:
+                        leap=True
+                  elif year%100==0:
+                        leap=False
+      
+      
+     
+    
+    # Write your logic here
+    
+      return leap
+
+year = int(input())
+print(is_leap(year))
+
+
+
+n = int(input())
+for n in range(n+1):
+      if n>=1 and n<=150:
+            print(n,end='')
+
+
+
+
+
+
+
+s = input()
+dupple=()
+s.split()
+
+for s in dupple:
+      dupple=s.split()
+      dupple.append(s)
+print(len(s))
+print(f'{s}')
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
