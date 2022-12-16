cff4378655f0240aec5e451ac1a7458181f46666
ehsanmorgan <ehsanmorgan1994@gmail.com>
2022-12-17 00:45:32 +0100

    add filter,map

---
+++ filter,map/filter,map.py
@@ -0,0 +1,32 @@
+
+names=('ehsan','ahmad','hasn','mahmoud')
+n=[x.title() for x in names ]
+print(n)
+
+
+
+
+
+names1=[]
+for x in names:
+      names1.append(x.upper())
+print(names1)
+
+
+def ehsan(y):
+      return y.lower()
+names4=filter(ehsan,names)
+print(list(names4))
+
+
+
+def mytitle(x):
+      return x.title()
+names3=map(mytitle,names)
+print(list(names3))
+
+
+
+
+no_duple={'my','my', 'name', 'name', 'is', 'is', 'is', 'ehsan', 'ehsan', 'ehsan'}
+print(no_duple)
